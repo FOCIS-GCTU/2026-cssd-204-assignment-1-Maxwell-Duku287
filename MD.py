dafa24d057d5e0197d798103056e38ecd59ae06d
Maxwell-Duku287 <2425400296@live.gctu.edu.gh>
2026-06-01 19:01:06 +0000

    Add files via upload

---
+++ MD.py
@@ -0,0 +1,38 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Maxwell Duku
+# STUDENT ID: 2425400296
+#GROUP :A
+# Email: 2425400296@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor,Maxwell Duku, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Prints my initials DOL in large stylized letters.
+
+    print()
+
+    print("...MD")
+
+    print()
+
+    print("...MMMM....MMMM........DDDDDDDD.........")
+    print("...MMMM....MMMM........DDDDDDDDDD.......")
+    print("...MM.MM..MM.MM........DD.......DDD.....")
+    print("...MM.MM..MM.MM........DD........DD.....")
+    print("...MM.MM..MM.MM........DD........DD.....")
+    print("...MM..MMMM..MM........DD........DD.....")
+    print("...MM..MMMM..MM........DD........DD.....")
+    print("...MM...MM...MM........DD.......DDD.....")
+    print("...MM...MM...MM...**...DDDDDDDDDD.....**")
+    print("...MM...MM...MM...**...DDDDDDDD.......**")
+
+    print()
+
+
+main()
